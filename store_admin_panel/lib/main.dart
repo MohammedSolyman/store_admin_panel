3f8584d60f447c62d70d22ef718e6cfe9bf7cf49
MohammedSolyman <MohammedSolyman10@gmail.com>
2023-08-01 15:08:41 +0300

    firebase and depencies and assets added

--- store_admin_panel/lib/main.dart
+++ store_admin_panel/lib/main.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainApp extends StatefulWidget {
+  MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Row(
+            children: [
+              Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.red,
+                  )),
+              Expanded(
+                  flex: 4,
+                  child: Container(
+                    color: Colors.yellow,
+                  ))
+            ],
+          ),
         ),
       ),
     );
